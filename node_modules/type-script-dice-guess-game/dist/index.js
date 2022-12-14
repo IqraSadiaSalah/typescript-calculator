f8b847bd88864f74e4fb7ad4d4de25d7b0d38c79
iqra <iqrasadia505@gmail.com>
2022-12-14 15:51:53 +0500

    committed

---
+++ node_modules/type-script-dice-guess-game/dist/index.js
@@ -0,0 +1,64 @@
+#! /usr/bin/env node
+import figlet from "figlet";
+import chalk from "chalk";
+import inquirer from "inquirer";
+import chalkAnimation from "chalk-animation";
+import randomeInteger from "random-int";
+import gradient from 'gradient-string';
+import { createSpinner } from "nanospinner";
+import PromptSync from "prompt-sync";
+const sleep = (ms = 1000) => new Promise((r) => setTimeout(r, ms));
+const sleep2 = (ms = 500) => new Promise((r) => setTimeout(r, ms));
+function Intro_screen() {
+    console.log(gradient('cyan', 'pink').multiline(figlet.textSync(`Welcome to the \n\nDice Roll Guessing Game !\n`), { interpolation: 'hsv' }) + '\n');
+}
+Intro_screen();
+async function welcome() {
+    const rainbowTitle = chalkAnimation.rainbow('This Game is Developed By Ahmed Ali Ansari PIAIC 171908');
+    await sleep();
+    rainbowTitle.stop();
+}
+const answers = await inquirer.prompt({
+    name: 'player_name',
+    type: 'input',
+    message: 'What is your name?',
+    default() {
+        return 'Player';
+    },
+});
+await welcome();
+function winner() {
+    figlet(`Congrats , ${answers.player_name} !\n`, (_err, data) => {
+        console.log(gradient.pastel.multiline(data) + '\n');
+    });
+    sleep();
+}
+var points;
+points = 0;
+do {
+    var computer_guess = randomeInteger(0, 6);
+    var answer = await inquirer.prompt([
+        {
+            name: "guess",
+            type: "number",
+            message: "Computer Just Rolled A dice ! whats your Guess ? "
+        },
+    ]);
+    const spinner = createSpinner('Checking answer...').start();
+    await sleep2();
+    if (!(answer.guess === computer_guess)) {
+        spinner.error({ text: `\nSorry Mate ! Your Guess is ${chalk.bgRed('Wrong')} ${answers.player_name}. \n${chalk.magenta(`Better luck Next Time`)}` });
+        console.log(`\nComputer Guess : ${chalk.blue(computer_guess)} Does Not Matched with Your Guess : ${chalk.blue(answer.guess)} `);
+    }
+    else {
+        spinner.success({ text: `\nNice work ${chalk.bgCyan(answers.player_name)}. You'r Lucky ` });
+        points += 10;
+        console.log(chalk.bgGreen(`\nYou Have Just earned `));
+        console.log(`\nPoints :  ${chalk.yellow(points)}  🎉`);
+        console.log(`\nComputer Guess : ${chalk.blue(computer_guess)} Matched with Your Guess : ${chalk.blue(answer.guess)} `);
+        winner();
+    }
+    const prompt1 = PromptSync();
+    var check = prompt1("want to Continue y or n ? ");
+} while (!(check == 'n'));
+console.log("\nThankYou FOr playing the Game Comback Leter !");
---
+++ project.mjs
@@ -0,0 +1,109 @@
+#! /usr/bin/env node
+// import chalk from 'chalk';
+import inquirer from 'inquirer';
+// import gradient from 'gradient-string';
+import chalkAnimation from 'chalk-animation';
+// import figlet from 'figlet';
+import { createSpinner } from 'nanospinner';
+let playerName;
+const sleep = () => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, 2000);
+    });
+};
+async function invitation() {
+    let rainbowText = chalkAnimation.rainbow("Are you here to use Nodejs Caluclator? \n  Created By Iqra");
+    await sleep();
+    rainbowText.stop();
+}
+async function askPlayerName() {
+    const answers = await inquirer.prompt({
+        name: 'player_name',
+        type: "input",
+        message: "What is your name?",
+        default() {
+            return "Name";
+        }
+    });
+    playerName = answers.player_name;
+}
+async function questions() {
+    const ans = await inquirer.
+        prompt([
+        {
+            name: 'question_1',
+            type: 'number',
+            message: 'Please enter a number:',
+            default() {
+                return "0";
+            }
+        },
+        {
+            name: 'question_2',
+            type: 'number',
+            message: 'Please enter a second number:',
+            default() {
+                return "0";
+            }
+        },
+        {
+            name: 'operator',
+            type: 'list',
+            message: 'Please select an operator: ',
+            choices: ['Addition', 'Subraction', 'Multiplication', 'Division', 'Modulus']
+        }
+    ]);
+    const spinner = createSpinner('Calculatuing...').start();
+    await sleep();
+    if (ans.operator == 'Addition') {
+        let result = `${ans.question_1} + ${ans.question_2} = ${ans.question_1 + ans.question_2}`;
+        spinner.success({
+            text: `Your answer is: ${result}`
+        });
+    }
+    else if (ans.operator == 'Subraction') {
+        let result = `${ans.question_1} - ${ans.question_2} = ${ans.question_1 - ans.question_2}`;
+        spinner.success({
+            text: `Your answer is: ${result}`
+        });
+    }
+    else if (ans.operator == 'Multiplication') {
+        let result = `${ans.question_1} * ${ans.question_2} = ${ans.question_1 * ans.question_2}`;
+        spinner.success({
+            text: `Your answer is: ${result}`
+        });
+    }
+    else if (ans.operator == 'Division') {
+        let result = `${ans.question_1} / ${ans.question_2} = ${ans.question_1 / ans.question_2}`;
+        spinner.success({
+            text: `Your answer is: ${result}`
+        });
+    }
+    else if (ans.operator == 'Modulus') {
+        let result = `${ans.question_1} % ${ans.question_2} = ${ans.question_1 % ans.question_2}`;
+        spinner.success({
+            text: `Your answer is: ${result}`
+        });
+    }
+    else {
+        spinner.error({ text: `Please enter valid value!` });
+        //process.exit(1)
+    }
+}
+async function continuegame() {
+    do {
+        await questions();
+        var again = await inquirer.prompt({
+            name: 'restart',
+            type: 'input',
+            message: "Do you want to continue?",
+            default() {
+                return playerName;
+            }
+        });
+    } while (again.restart == 'y' || again.restart == 'Y' || again.restart == 'YES' || again.restart == 'yes');
+}
+console.clear();
+await invitation();
+await askPlayerName();
+await continuegame();
